790afaaa0fbde17464e9e3845333aedb8cd2cfe8
jakubskiba <jakubskibakrk@gmail.com>
2018-04-13 12:03:04 +0200

    remove redundant imports

--- src/main/java/com/jskiba/App.java
+++ src/main/java/com/jskiba/App.java
@@ -1,18 +1,9 @@
 package com.jskiba;
 
 import com.jskiba.controller.GameController;
-import com.jskiba.controller.PlayerControllerFactory;
-import com.jskiba.service.BoardValidator;
-import com.jskiba.view.View;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 
 @Configuration
 @ComponentScan
